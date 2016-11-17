22ceac94ae1e62838af22b8eca6644d5df0f748a
appsssec <apps.ssec@gmail.com>
2016-11-17 18:35:51 +0530

    siteupdate

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -1,4 +1,4 @@
-// (C) Copyright 2015 Martin Dougiamas
+﻿// (C) Copyright 2015 Martin Dougiamas
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -44683,10 +44683,10 @@ angular.module('mm.core')
     "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch","en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語","nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "pt": "Português - Portugal", "ro": "Română", "ru": "Русский", "sv": "Svenska", "tr" : "Türkçe", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
     "wsservice" : "moodle_mobile_app",
     "wsextservice" : "local_mobile",
-    "demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
+    "demo_sites": {"student": {"url": "https://ssec.org.in/moodle30/", "username": "student", "password": "moodle"}, "teacher": {"url": "https://ssec.org.in/moodle30/", "username": "teacher", "password": "moodle"}, "cva": {"url": "https://ssec.org.in/moodle30/", "username": "student", "password": "student"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "",
+    "siteurl": "https://ssec.org.in/moodle30/",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false"
 }
